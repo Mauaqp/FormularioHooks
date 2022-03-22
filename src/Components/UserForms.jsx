5fc63192a02cb4ae010b44ccf292f3edb619422e
Mauaqp <peraltilla93@hotmail.com>
2022-03-22 17:18:04 -0500

    avance con conditionals

--- src/Components/UserForms.jsx
+++ src/Components/UserForms.jsx
@@ -1,10 +1,12 @@
 import React from  'react';
     
 const UserForms = props => {
+    // Diccionario de App.js desestructurado
     const {inputs, setInputs} = props;
-    // Function onChange
+    // Function onChange capturará el evento desde el cliente y lo pondra en setInputs
     const onChange = e => {
         setInputs({
+            //Spread operator trae todo el state y evita que se sobreescriba y borren los demás
             ...inputs,
             [e.target.name] : e.target.value,
 
@@ -16,6 +18,11 @@ const UserForms = props => {
             <div>
                 <label htmlFor="firstName">First Name: </label> 
                 <input type="text" name="firstName" onChange={onChange} />
+                <p>
+                    {inputs.firstName.length > 0 && inputs.firstName.length < 2
+                    ? "First name must be at least 2 characters."
+                    : ""}
+                </p>
             </div>
             <div>
                 <label htmlFor="lastName">Last Name: </label> 
@@ -32,6 +39,13 @@ const UserForms = props => {
             <div>
                 <label htmlFor="confirmPassword">Confirm Password: </label>
                 <input type="password" name="confirmPassword" onChange={onChange} />
+                <p>
+                    {inputs.confirmPassword.length > 0 &&
+                    inputs.confirmPassword.length < 8
+                    ? "Password must be at least 8 characters."
+                    : ""}
+                    {inputs.password !== inputs.confirmPassword ? "Passwords do not match." : ""}
+                </p>
             </div>
         </form>
     );
