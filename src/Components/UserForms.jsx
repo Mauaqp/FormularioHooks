2014b50a09f48569e38d580c5abcb8cb3227e67c
Mauaqp <peraltilla93@hotmail.com>
2022-03-23 12:22:28 -0500

    Avance actividad Core

--- src/Components/UserForms.jsx
+++ src/Components/UserForms.jsx
@@ -27,14 +27,29 @@ const UserForms = props => {
             <div>
                 <label htmlFor="lastName">Last Name: </label> 
                 <input type="text" name ="lastName" onChange={onChange} />
+                <p>
+                    {inputs.lastName.length > 0 && inputs.lastName.length < 2
+                    ? "Last name must be at least 2 characters."
+                    : ""}
+                </p>
             </div>
             <div>
                 <label htmlFor="email">Email Address: </label> 
                 <input type="text" name="email" onChange={onChange} />
+                <p>
+                    {inputs.email.length > 0 && inputs.email.length < 2
+                    ? "Email name must be at least 2 characters."
+                    : ""}
+                </p>
             </div>
             <div>
                 <label htmlFor="password">Password: </label>
                 <input type="password" name="password"  onChange={onChange} />
+                <p>
+                    {inputs.password.length > 0 && inputs.password.length < 8
+                    ? "Password must be at least 8 characters."
+                    : ""}
+                </p>
             </div>
             <div>
                 <label htmlFor="confirmPassword">Confirm Password: </label>
